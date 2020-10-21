760ccce65b5a2562aaf5257cce28d24ff8302dad
LPMartinez <lpmartinez1109@gmail.com>
2020-10-21 10:14:21 -0500

    Adding constructor elements to start building display on site

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(props){
+    super(props)
+    console.log("This is a test")
+    const movies  =[
+      {id: 0, title:"Hello this is a test", overview:"this is a test!"}
+    ]
+    this.state = {
+      rows: [
+        <p key="1">This is a test</p>,
+        <p key="2">This is a test</p>,
+        <p key="3">This is a test</p>
+      ]
+    }
+  }
+  render(){
   return (
     <div className="App">
      <table className="titleHeader">
        <tbody>
          <tr>
            <td>
-            <img width="50" src="movie_icon.png"/>
+            <img width="50" src="movie_icon.png" />
            </td>
            <td width="8"/>
            <td>
@@ -19,8 +33,9 @@ function App() {
        </tbody>
      </table>
      <input className="titleSearch" placeholder="Enter search here"/>
+     {this.state.rows}
     </div>
   );
+  }
 }
-
 export default App;
